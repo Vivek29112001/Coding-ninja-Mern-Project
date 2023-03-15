8fd49511f9f8ee58e279a775f347fa831e58581d
viveksharma2911 <92039146+Vivek29112001@users.noreply.github.com>
2023-03-15 08:33:22 -0400

    Add files via upload

---
+++ catch me if you can/script.js
@@ -0,0 +1,32 @@
+var box = document.getElementById("target-div");
+
+var maxHeight= window.innerHeight;
+var maxWidth = window.innerWidth;
+
+box.addEventListener('mouseover', function(){
+    var curr = box.getBoundingClientRect();
+    console.log('move over fired');
+    var pos = getPos(curr.height, curr.width);
+
+    box.style.top = pos.y + "px";
+    box.style.left = pos.x + "px";
+
+    console.log("reached end of event");
+});
+
+function getPos(currX, currY){
+    console.log("getPos fixed");
+    var newX = Math.random() * maxWidth + 1 - currX;
+    var newY = Math.random() * maxHeight + 1 - currY;
+
+    if(newX<0)
+    newX=0;
+
+    if(newY<0)
+    newY=0;
+
+    return{
+        x: newX,
+        y: newY
+    };
+}
